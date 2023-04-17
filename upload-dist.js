7c997a1f65d57a19cd494067c3ab5aa093f555df
gbnm <goncalo.martins@outsystems.com>
2023-04-17 21:55:24 +0100

    Updated script and created JSON
    Updated script and created sample JSON

--- upload-dist.js
+++ upload-dist.js
@@ -1,8 +1,11 @@
 const request = require('request');
 const fs = require('fs');
-
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const distPath = 'dist/';
+const endpoint = 'https://personal-oo8cwon9.outsystemscloud.com/ComponentsBuilder_API/rest/ComponentsBuilder/BuildModule';
+const guid = uuidv4(); // generate a new GUID
 
 fs.readdir(distPath, function(err, files) {
   if (err) {
@@ -10,17 +13,23 @@ fs.readdir(distPath, function(err, files) {
     return;
   }
 
+
+  console.log('Files in ' + distPath + ' for the requestId =' + guid + ':');
+
   files.forEach(function(file) {
-    const filePath = distPath + file;
+
+    const filePath = path.join(distPath, file);
     const fileStream = fs.createReadStream(filePath);
 
     const options = {
-      url: 'https://personal-oo8cwon9.outsystemscloud.com/ComponentsBuilder_API/rest/ComponentsBuilder/BuildModule',
+      url: endpoint,
       method: 'POST',
-      formData: {
-        file: fileStream,
-        filename: file
-      }
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Filename': file,
+        'GUID': guid
+      },
+      body: fileStream
     };
 
     request(options, function(err, res, body) {
